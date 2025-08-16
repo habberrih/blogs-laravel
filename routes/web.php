708faf05ca5df030867cb4703a8a5702071627df
habberrih <abdul2013ly@gmail.com>
2025-08-16 19:13:58 +0200

    feat(post-route): add edit and update routes

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::get('/posts', [PostController::class, 'index'])->name('posts.index'); // 
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::post('/posts', [PostController::class,  'store'])->name('posts.store');
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
