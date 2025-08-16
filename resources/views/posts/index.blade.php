fe7b975ad44ed2701864c09ef47fb69d3dd9bb2b
habberrih <abdul2013ly@gmail.com>
2025-08-16 19:32:10 +0200

    feat(posts-views-index): add a new form with button for deleting an item

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -25,7 +25,12 @@
                 <td>
                     <a href='{{route('posts.show', $post['id'])}}' class="btn btn-info">View</a>
                     <a href="{{route('posts.edit', $post['id'])}}" class="btn btn-primary">Edit</a>
-                    <a href="#" class="btn btn-danger">Delete</a>
+
+                    <form style="display: inline" method="POST" action="{{route('posts.destroy', $post['id'])}}">
+                    @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
                 </td>
             </tr>
         @endforeach
