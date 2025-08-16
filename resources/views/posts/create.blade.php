f73d15634c7c431ec8d5eeb1fc256752c5651622
habberrih <abdul2013ly@gmail.com>
2025-08-16 18:30:14 +0200

    feat(create.blade.php): add csrf tag and add names to the fields

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,18 +2,19 @@
 @section('title') Create  Post @endsection('title')
 @section('content')
 
-    <form>
+    <form method="POST" action="{{route('posts.store')}}">
+        @csrf
         <div class="mb-3">
             <label class="form-label">Title</label>
-            <input type="text" class="form-control">
+            <input type="text" name="title" class="form-control">
         </div>
         <div class="mb-3">
             <label class="form-label">Description</label>
-            <textarea class="form-control" rows="3"></textarea>
+            <textarea name="description" class="form-control" rows="3"></textarea>
         </div>
         <div class="mb-3">
             <label class="form-label">Creator</label>
-            <select class="form-control">
+            <select name="post_creator" class="form-control">
                 <option value="1">Ahmed</option>
                 <option value="2">Mohammed</option>
             </select>
