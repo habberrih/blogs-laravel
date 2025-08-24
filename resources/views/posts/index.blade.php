26a819d57e0281175cec6e470b2de40ff9266c77
habberrih <abdul2013ly@gmail.com>
2025-08-24 20:14:19 +0200

    refactor(posts-index.blade.php): use object operator to access data

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -18,15 +18,15 @@
         <tbody>
         @foreach($posts as $post)
             <tr>
-                <th scope="row">{{$post['id']}}</th>
-                <td>{{$post['title']}}</td>
-                <td>{{$post['posted_by']}}</td>
-                <td>{{$post['created_at']}}</td>
+                <th scope="row">{{$post -> id}}</th>
+                <td>{{$post -> title}}</td>
+                <td>{{$post -> posted_by}}</td>
+                <td>{{$post -> created_at}}</td>
                 <td>
-                    <a href='{{route('posts.show', $post['id'])}}' class="btn btn-info">View</a>
-                    <a href="{{route('posts.edit', $post['id'])}}" class="btn btn-primary">Edit</a>
+                    <a href='{{route('posts.show', $post->id)}}' class="btn btn-info">View</a>
+                    <a href="{{route('posts.edit', $post->id)}}" class="btn btn-primary">Edit</a>
 
-                    <form style="display: inline" method="POST" action="{{route('posts.destroy', $post['id'])}}">
+                    <form style="display: inline" method="POST" action="{{route('posts.destroy', $post->id)}}">
                     @csrf
                         @method('DELETE')
                         <button type="submit" class="btn btn-danger">Delete</button>
