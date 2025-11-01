07ad7113d4014cd9ca174a5260682d72dbe42341
habberrih <abdul2013ly@gmail.com>
2025-11-01 23:47:49 +0200

    refactor: add type to method

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -12,7 +13,7 @@ class Post extends Model
         'user_id',
     ];
 
-    public function user() {
+    public function user(): BelongsTo {
         return $this->belongsTo(User::class);
     }
 }
