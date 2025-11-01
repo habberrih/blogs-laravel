170846a78bfa3b1de5b0da7c91b22bbde6093407
habberrih <abdul2013ly@gmail.com>
2025-11-01 23:58:54 +0200

    refactor: add if condition to show the user name that belongs to post

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -18,7 +18,7 @@
             <label class="form-label">New Creator</label>
             <select name="post_creator" class="form-control">
                 @foreach($users as $user)
-                <option value="{{$user -> id}}">{{$user -> name}}</option>
+                <option @if($user->id == $post->user_id) selected @endif value="{{$user -> id}}">{{$user -> name}}</option>
                 @endforeach
 
             </select>
